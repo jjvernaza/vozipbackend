df2100b552241a538fc464d05c2b409dcd00c52d
jjvernaza <jjvernazamayor@gmail.com>
2023-03-02 09:58:04 -0500

    reescribimos los archivos para tener orden y agregue el .gitignore

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { fn } = require('sequelize');
-const app = express();
-
-const postsRoute = require('./routes/posts');
-app.use("/posts", postsRoute);
-
-module.exports = app;
